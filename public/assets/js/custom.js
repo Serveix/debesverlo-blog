3f746c1a187b50842c3c96d0d1a803340a3b86d7
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2019-05-27 23:26:13 -0500

    update

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -59,11 +59,3 @@ wow = new WOW({
     offset: 100
 });
 wow.init();
-
-$(window).load(function() { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    // $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(100).css({
-        'overflow': 'visible'
-    });
-})
