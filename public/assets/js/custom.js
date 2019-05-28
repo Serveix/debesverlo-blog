cc1d5aad01b1ecfd84fbfd699de9fc148947a50b
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2019-05-27 23:19:25 -0500

    Improvements

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -62,7 +62,7 @@ wow.init();
 
 jQuery(window).load(function() { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    // $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(100).css({
         'overflow': 'visible'
     });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
 
 </head>
 <body>
-<div id="preloader">
-    <div id="status">&nbsp;</div>
-</div>
+{{--<div id="preloader">--}}
+    {{--<div id="status">&nbsp;</div>--}}
+{{--</div>--}}
 <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
 <div class="container">
     <header id="header">
@@ -114,14 +114,17 @@
         </div>
     </footer>
 </div>
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/wow.min.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/slick.min.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.li-scroller.1.0.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.newsTicker.min.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.fancybox.pack.js') }}"></script>
-<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script>
+    document.addEventListener('touchstart', onTouchStart, {passive: true});
+</script>
+<script src="{{ secure_asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/wow.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/slick.min.js') }}"></script>
+{{--<script src="{{ secure_asset('assets/js/jquery.li-scroller.1.0.js') }}"></script>--}}
+<script src="{{ secure_asset('assets/js/jquery.newsTicker.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/jquery.fancybox.pack.js') }}"></script>
+<script src="{{ secure_asset('assets/js/custom.js') }}"></script>
 
 <!-- Main Quill library -->
 {{--    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>--}}
