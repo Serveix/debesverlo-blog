2a11e76b6e8d8088599c0dac12d975348a2e73ea
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2019-05-30 01:44:10 -0500

    GAnalytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,17 @@
               enable_page_level_ads: true
          });
     </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141100384-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-141100384-1');
+    </script>
+
 </head>
 <body>
     <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
